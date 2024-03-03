0772898600661c5677b2feebab4e0b606a91f250
Hamza DAMIRI <hamza.damiri@gmail.com>
2024-03-03 22:10:25 +0100

    Initial commit

---
+++ src/main/java/ma/dev7hd/activite_pratique_n2/entities/Patient.java
@@ -0,0 +1,23 @@
+package ma.dev7hd.activite_pratique_n2.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "PATIENTS")
+@Data @NoArgsConstructor @AllArgsConstructor
+public class Patient {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    //Q3: Création de l'entité JPA Patient
+    private Long id;
+    private String nom;
+    private Date dateNaissance;
+    private int score;
+    private boolean malade;
+}
